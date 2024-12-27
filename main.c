e01df13c0793b2e5f2ae536f479e4a173ffa015e
conspira <conspira@nexon.co.kr>
2024-12-27 10:27:32 +0900

    00

--- main.c
+++ main.c
@@ -2,33 +2,7 @@
 
 int main()
 {
-    print("main 1");
-    print("main 2");
-    print("main 3");
-    print("main 4");
-    print("main 5");
-    print("main 6");
-    print("main 7");
+    print("main 00");
 
-    print("main xx");
-    print("hotfix 1");
-    print("feature 1");
-    print("feature 2");
-    print("feature 3");
-    print("main 3");
-    print("main 4");
-    print("main 5");
-    print("main 6");
-    print("main 7");
-
-    print("feature 1");
-    print("feature 2");
-    print("feature 3");
-    print("main xx");
-    print("hotfix 1");
-    print("main 6");
-    print("main 7");
-
-    print("main xx");
     return 0;
 }
